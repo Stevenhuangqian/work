2439529107500b0572da8ed843febc3ff8b309c7
Stevenhuangqian <qq42289141@gmail.com>
2018-01-28 16:32:09 +0800

    登录校验的注解及对应拦截器

---
+++ src/main/java/com/core/work/annotation/login/LoginRequired.java
@@ -0,0 +1,18 @@
+package com.core.work.annotation.login;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+/**
+ * 一个登录验证注解
+ * @author 黄谦
+ *
+ */
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LoginRequired {
+    
+}
---
+++ src/main/java/com/core/work/interceptor/LoginInterceptor.java
@@ -0,0 +1,57 @@
+package com.core.work.interceptor;
+
+import java.lang.annotation.Annotation;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import com.core.work.annotation.login.LoginRequired;
+
+/**
+ * 登录验证拦截器
+ */
+public class LoginInterceptor extends HandlerInterceptorAdapter {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (handler instanceof HandlerMethod) {
+            LoginRequired loginRequired = findAnnotation((HandlerMethod) handler, LoginRequired.class);
+            //没有声明需要权限,或者声明不验证权限
+            if(loginRequired==null){
+                return true;
+            }else{
+                String token=request.getHeader("token");
+                if(StringUtils.isEmpty(token)){
+                    token=request.getParameter("token");
+                }
+                //TODO 在这里实现自己的权限验证逻辑
+                if(!StringUtils.isEmpty(token)){//如果验证成功返回true（这里直接写false来模拟验证失败的处理）
+                    return true;
+                }else{//如果验证失败
+
+                    //response.getWriter().write("您还未登录");
+                    return false;
+                }       
+            }
+        }else{
+            return true;
+        }
+    }
+
+    /**
+     * 登录注解拦截器
+     * @param handler
+     * @param annotationType
+     * @param <T>
+     * @return
+     */
+    private <T extends Annotation> T findAnnotation(HandlerMethod handler, Class<T> annotationType) {
+        T annotation = handler.getBeanType().getAnnotation(annotationType);
+        if (annotation != null) return annotation;
+        return handler.getMethodAnnotation(annotationType);
+    }
+}
